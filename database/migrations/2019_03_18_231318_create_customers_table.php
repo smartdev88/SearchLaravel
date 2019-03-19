581a9bf1f65abd517e1194ebbceb932b864bc414
yassine yass <lagyassine88@gmail.com>
2019-03-19 12:25:52 +0100

    Live search in laravel using AJAX

---
+++ database/migrations/2019_03_18_231318_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('CustomerID');
+            $table->string('CustomerName');
+            $table->string('Address');
+            $table->string('City');
+            $table->string('PostalCode',30);
+            $table->string('Country',100);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
